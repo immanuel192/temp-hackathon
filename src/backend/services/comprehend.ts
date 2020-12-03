6b55ecb106bae600235c3c73928aebfe9d61955e
Jason Shin <visualbbasic@gmail.com>
2020-12-03 13:42:43 +1100

    wip comprehend

--- src/backend/services/comprehend.ts
+++ src/backend/services/comprehend.ts
@@ -1,6 +1,19 @@
-import { IAWSComprehend, IComprehendScore } from "@/backend/services/interfaces";
+import { IAWSComprehend } from "./interfaces";
+import {IComprehendScore} from "@/share";
+import { config, Comprehend } from 'aws-sdk'
+
+config.update({ region: 'eu-west-1' }); // Whatever region for now.
 
 export default class AWSComprehend implements IAWSComprehend {
+  private comprehend: Comprehend
+
+  constructor(props) {
+    // @ts-ignore
+    super(props);
+    this.comprehend = new Comprehend()
+  }
+
+
   public analyse(text: string[]): Promise<IComprehendScore> {
     return Promise.resolve({
       mixed: 0.1,
